cd08eb8387c49e2c13cc0bdda2631f9e8a85f2d4
epepepelephant <2930106762@qq.com>
2026-04-16 22:22:05 +0800

    Deployed 55f38dc with MkDocs version: 1.6.1

--- javascripts/favicon.js
+++ javascripts/favicon.js
@@ -1,19 +1,7 @@
-// 强制设置 favicon - 在页面加载完成后执行
+// 强制设置 favicon
 document.addEventListener('DOMContentLoaded', function() {
-  // 创建新的 favicon 链接
-  var link = document.createElement('link');
-  link.rel = 'icon';
-  link.href = 'assets/images/favicon.ico';
-  link.type = 'image/x-icon';
-
-  // 移除旧的 favicon 链接
-  var oldLinks = document.querySelectorAll('link[rel="icon"], link[rel="shortcut icon"], link[rel="apple-touch-icon"]');
-  oldLinks.forEach(function(el) {
-    el.parentNode.removeChild(el);
+  var links = document.querySelectorAll('link[rel="icon"]');
+  links.forEach(function(link) {
+    link.href = '../../assets/images/favicon.ico';
   });
-
-  // 添加新的 favicon
-  document.head.appendChild(link);
-
-  console.log('Favicon set to favicon.ico');
 });
